8567677671ee963e7366f1c3440227843bc2d3c0
Mark O'deBeast <80601021+Skatan666@users.noreply.github.com>
2022-07-16 21:56:47 +0200

    Add files via upload

---
+++ Kelvin Weather/Components/app.js
@@ -0,0 +1,13 @@
+//variable kelvin is set to 293
+
+let kelvin = 293
+
+//subtracting 273 from kelvin assign value to celsius
+celsius = kelvin - 273
+
+//result of celsius multiplied by nine divided by five plus thirty two
+
+fahrenheit = celsius * (9/5) + 32
+
+//log fahrenheit results on to the console
+console.log(Math.floor(fahrenheit));
